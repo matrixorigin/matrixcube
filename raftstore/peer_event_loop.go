5e8a2ae7d73d87701ea182d434e5bb2c4d30003c
zhangxu <zhangxu19830126@gmail.com>
2019-12-17 09:37:37 +0800

    Fix raftstore stop

--- raftstore/peer_event_loop.go
+++ raftstore/peer_event_loop.go
@@ -112,8 +112,15 @@ func (pr *peerReplica) readyToServeRaft(ctx context.Context) {
 			continue
 		}
 
+		stop := false
+		select {
+		case <-ctx.Done():
+			stop = true
+		default:
+		}
+
 		_, err := pr.events.Get()
-		if err != nil {
+		if err != nil || stop {
 			pr.metrics.flush()
 			pr.actions.Dispose()
 			pr.ticks.Dispose()
