c8149cdc0e6799db8bbeda154faffd1ad57efd46
zhangxu <zhangxu19830126@gmail.com>
2020-01-21 10:13:50 +0800

    Change DataStorage extends Metadatastorage

--- storage/storage_data.go
+++ storage/storage_data.go
@@ -2,8 +2,8 @@ package storage
 
 // DataStorage responsible for maintaining the data storage of a set of shards for the application.
 type DataStorage interface {
-	// RangeDelete remove data in [start,end)
-	RangeDelete(start, end []byte) error
+	MetadataStorage
+
 	// SplitCheck Find a key from [start, end), so that the sum of bytes of the value of [start, key) <=size,
 	// returns the current bytes in [start,end), and the founded key
 	SplitCheck(start []byte, end []byte, size uint64) (currentSize uint64, splitKey []byte, err error)
@@ -11,7 +11,4 @@ type DataStorage interface {
 	CreateSnapshot(path string, start, end []byte) error
 	// ApplySnapshot apply a snapshort file from giving path
 	ApplySnapshot(path string) error
-
-	// Close close the storage
-	Close() error
 }
