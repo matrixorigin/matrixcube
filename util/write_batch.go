e8f1b595175f5a8679ea77eda1a587a2d6bd71ab
zhangxu <zhangxu19830126@gmail.com>
2020-02-17 09:00:49 +0800

    Add TTL for write batch interface

--- util/write_batch.go
+++ util/write_batch.go
@@ -6,4 +6,6 @@ type WriteBatch interface {
 	Delete(key []byte) error
 	// Set set the key-value pair
 	Set(key []byte, value []byte) error
+	// SetWithTTL set the key-value paire with a ttl in seconds
+	SetWithTTL(key []byte, value []byte, ttl int64) error
 }
