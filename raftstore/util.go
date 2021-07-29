fc2b9770cb410be7a810f7682e5f4e11299e3915
fagongzi <zhangxu19830126@gmail.com>
2021-07-29 09:38:52 +0800

    Add license head (#63)

--- raftstore/util.go
+++ raftstore/util.go
@@ -1,4 +1,4 @@
-// Copyright 2016 DeepFabric, Inc.
+// Copyright 2020 MatrixOrigin.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,7 +10,6 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package raftstore
 
 import (
