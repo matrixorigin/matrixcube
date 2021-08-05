4374653646281f9520f34c344afb751628e3cfa0
zhangxu <zhangxu19830126@gmail.com>
2021-08-05 17:21:37 +0800

    Fix #61 support shards pool

---
+++ raftstore/store_shards_pool.go
@@ -0,0 +1,408 @@
+package raftstore
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/fagongzi/goetty/buf"
+	"github.com/fagongzi/util/format"
+	"github.com/fagongzi/util/protoc"
+	"github.com/matrixorigin/matrixcube/components/prophet"
+	"github.com/matrixorigin/matrixcube/components/prophet/config"
+	"github.com/matrixorigin/matrixcube/components/prophet/metadata"
+	"github.com/matrixorigin/matrixcube/components/prophet/pb/metapb"
+	"github.com/matrixorigin/matrixcube/components/prophet/storage"
+	"github.com/matrixorigin/matrixcube/pb/bhmetapb"
+)
+
+var (
+	errNoIdleShard = errors.New("no idle shard")
+)
+
+// ShardsPool is a shards pool, it will always create shards until the number of available shards reaches the
+// value specified by `capacity`, we called these `Idle Shards`.
+//
+// The pool will create a Job in the prophet. Once a node became the prophet leader,  shards pool job will start,
+// and stop if the node became the follower, So the job can be executed on any node. It will use prophet client to
+// create shard after the job starts.
+type ShardsPool interface {
+	// Alloc alloc a shard from shards pool, returns error if no idle shards left. The `purpose` is used to avoid
+	// duplicate allocation.
+	Alloc(group uint64, purpose []byte) (bhmetapb.AllocatedShard, error)
+}
+
+func (s *store) CreateResourcePool(pools ...metapb.ResourcePool) (ShardsPool, error) {
+	err := s.pd.GetClient().CreateJob(metapb.Job{Type: metapb.JobType_CreateResourcePool, Content: protoc.MustMarshal(&metapb.ResourcePoolJob{
+		Pools: pools,
+	})})
+	if err != nil {
+		return nil, err
+	}
+
+	return s.shardPool, nil
+}
+
+// dynamicShardsPool a dynamic shard pool
+type dynamicShardsPool struct {
+	job    metapb.Job
+	ctx    context.Context
+	cancel context.CancelFunc
+	pd     prophet.Client
+	pdC    chan struct{}
+
+	mu struct {
+		sync.RWMutex
+
+		state   int
+		pools   bhmetapb.ShardsPool
+		createC chan struct{}
+	}
+}
+
+func newDynamicShardsPool() *dynamicShardsPool {
+	p := &dynamicShardsPool{pdC: make(chan struct{}, 1)}
+	config.RegisterJobProcessor(metapb.JobType_CreateResourcePool, p)
+	return p
+}
+
+func (dsp *dynamicShardsPool) setProphetClient(pd prophet.Client) {
+	dsp.pd = pd
+	close(dsp.pdC)
+}
+
+func (dsp *dynamicShardsPool) waitProphetClientSetted() {
+	if dsp.pd != nil {
+		return
+	}
+	<-dsp.pdC
+}
+
+func (dsp *dynamicShardsPool) Alloc(group uint64, purpose []byte) (bhmetapb.AllocatedShard, error) {
+	allocated := bhmetapb.AllocatedShard{}
+	retry := 0
+	for {
+		v, err := dsp.pd.ExecuteJob(metapb.Job{Type: metapb.JobType_CreateResourcePool}, protoc.MustMarshal(&bhmetapb.ShardsPoolCmd{
+			Type: bhmetapb.ShardsPoolCmdType_AllocShard,
+			Alloc: &bhmetapb.ShardsPoolAllocCmd{
+				Group:   group,
+				Purpose: purpose,
+			},
+		}))
+		if err == nil && len(v) > 0 {
+			protoc.MustUnmarshal(&allocated, v)
+			return allocated, nil
+		}
+
+		if retry > 0 {
+			if err != nil {
+				return allocated, err
+			}
+
+			// no idle shard left
+			if len(v) == 0 {
+				return allocated, errNoIdleShard
+			}
+		}
+
+		retry++
+		time.Sleep(time.Second)
+	}
+}
+
+func (dsp *dynamicShardsPool) Start(job metapb.Job, store storage.JobStorage, aware config.ResourcesAware) {
+	dsp.mu.Lock()
+	defer dsp.mu.Unlock()
+
+	if dsp.isStartedLocked() {
+		return
+	}
+
+	// load or init the job data
+	value, err := store.GetJobData(job)
+	if err != nil {
+		return
+	}
+	if len(value) > 0 {
+		dsp.mu.pools = bhmetapb.ShardsPool{}
+		protoc.MustUnmarshal(&dsp.mu.pools, value)
+	} else {
+		jobContent := &metapb.ResourcePoolJob{}
+		protoc.MustUnmarshal(jobContent, job.Content)
+
+		dsp.mu.pools.Pools = make(map[uint64]*bhmetapb.ShardPool)
+		for _, p := range jobContent.Pools {
+			dsp.mu.pools.Pools[p.Group] = &bhmetapb.ShardPool{
+				Capacity:    p.Capacity,
+				RangePrefix: p.RangePrefix,
+			}
+		}
+	}
+
+	dsp.mu.state = 1
+	dsp.job = job
+	dsp.mu.createC = make(chan struct{}, 8)
+	dsp.ctx, dsp.cancel = context.WithCancel(context.Background())
+	dsp.startLocked(dsp.ctx, dsp.mu.createC, store, aware)
+}
+
+func (dsp *dynamicShardsPool) Stop(job metapb.Job, store storage.JobStorage, aware config.ResourcesAware) {
+	dsp.mu.Lock()
+	defer dsp.mu.Unlock()
+
+	if !dsp.isStartedLocked() {
+		return
+	}
+
+	dsp.mu.state = 0
+	if dsp.cancel != nil {
+		dsp.cancel()
+		dsp.ctx = nil
+		dsp.cancel = nil
+	}
+}
+
+func (dsp *dynamicShardsPool) Remove(job metapb.Job, store storage.JobStorage, aware config.ResourcesAware) {
+	dsp.Stop(job, store, aware)
+}
+
+func (dsp *dynamicShardsPool) Execute(data []byte, store storage.JobStorage, aware config.ResourcesAware) ([]byte, error) {
+	if len(data) <= 0 {
+		return nil, errors.New("error execute data")
+	}
+
+	cmd := &bhmetapb.ShardsPoolCmd{}
+	protoc.MustUnmarshal(cmd, data)
+
+	dsp.mu.Lock()
+	defer dsp.mu.Unlock()
+
+	if !dsp.isStartedLocked() {
+		return nil, fmt.Errorf("job not started")
+	}
+
+	switch cmd.Type {
+	case bhmetapb.ShardsPoolCmdType_AllocShard:
+		return dsp.doAllocLocked(cmd.Alloc, store, aware)
+	default:
+		return nil, fmt.Errorf("invalid execute cmd %d", cmd.Type)
+	}
+}
+
+func (dsp *dynamicShardsPool) doAllocLocked(cmd *bhmetapb.ShardsPoolAllocCmd, store storage.JobStorage, aware config.ResourcesAware) ([]byte, error) {
+	group := cmd.Group
+	p := dsp.mu.pools.Pools[group]
+
+	// no idle shard left, trigger create, and return nil, client need to retry later
+	if p.Seq-p.AllocatedOffset == 0 {
+		dsp.triggerCreateLocked()
+		return nil, nil
+	}
+
+	// check whether the purpose has been allocated before
+	if len(p.AllocatedShards) > 0 {
+		for _, allocated := range p.AllocatedShards {
+			if bytes.Equal(allocated.Purpose, cmd.Purpose) {
+				dsp.triggerCreateLocked()
+				return protoc.MustMarshal(allocated), nil
+			}
+		}
+	}
+
+	old := dsp.cloneDataLocked()
+	id := uint64(0)
+	p.AllocatedOffset++
+	unique := dsp.unique(group, p.AllocatedOffset)
+	fn := func(res metadata.Resource) {
+		shard := res.(*resourceAdapter).meta
+		if shard.Unique == unique {
+			id = shard.ID
+		}
+	}
+	aware.ForeachWaittingCreateResources(fn)
+	if id == 0 {
+		aware.ForeachResources(group, fn)
+	}
+	if id == 0 {
+		logger.Fatalf("BUG: missing alloced shard")
+	}
+
+	allocated := &bhmetapb.AllocatedShard{
+		ShardID:     id,
+		AllocatedAt: p.AllocatedOffset,
+		Purpose:     cmd.Purpose,
+	}
+	p.AllocatedShards = append(p.AllocatedShards, allocated)
+	dsp.mu.pools.Pools[group] = p
+
+	if err := dsp.saveLocked(store); err != nil {
+		logger.Errorf("shards pool alloc failed with %+v, retry later",
+			err)
+		dsp.mu.pools = old
+		return nil, err
+	}
+
+	dsp.triggerCreateLocked()
+	return protoc.MustMarshal(allocated), nil
+}
+
+func (dsp *dynamicShardsPool) startLocked(ctx context.Context, c chan struct{}, store storage.JobStorage, aware config.ResourcesAware) {
+	dsp.triggerCreateLocked()
+	go func(ctx context.Context, c chan struct{}) {
+		logger.Infof("dynamic shards pool job started")
+		defer func() {
+			close(c)
+		}()
+
+		dsp.waitProphetClientSetted()
+
+		ticker := time.NewTicker(time.Minute)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				logger.Infof("dynamic shards pool job stopped")
+				return
+			case <-c:
+				logger.Infof("dynamic shards pool job maybeCreate")
+				dsp.maybeCreate(store)
+				logger.Infof("dynamic shards pool job maybeCreate completed")
+			case <-ticker.C:
+				logger.Infof("dynamic shards pool job gcAllocating")
+				dsp.gcAllocating(store, aware)
+			}
+		}
+	}(ctx, c)
+}
+
+func (dsp *dynamicShardsPool) isStartedLocked() bool {
+	return dsp.mu.state == 1
+}
+
+func (dsp *dynamicShardsPool) triggerCreateLocked() {
+	select {
+	case dsp.mu.createC <- struct{}{}:
+	default:
+	}
+}
+
+func (dsp *dynamicShardsPool) maybeCreate(store storage.JobStorage) {
+	dsp.mu.Lock()
+	defer dsp.mu.Unlock()
+
+	if !dsp.isStartedLocked() {
+		return
+	}
+
+	old := dsp.cloneDataLocked()
+	var creates []metadata.Resource
+	for g, p := range dsp.mu.pools.Pools {
+		if p.Seq == 0 ||
+			(int(p.Seq-p.AllocatedOffset) < int(p.Capacity) && len(creates) < 8) {
+			p.Seq++
+			creates = append(creates, NewResourceAdapterWithShard(bhmetapb.Shard{Group: g,
+				Start:  addPrefix(p.RangePrefix, p.Seq),
+				End:    addPrefix(p.RangePrefix, p.Seq+1),
+				Unique: dsp.unique(g, p.Seq)}))
+		}
+	}
+
+	if len(creates) > 0 {
+		err := dsp.pd.AsyncAddResources(creates...)
+		if err != nil {
+			logger.Errorf("shards pool create shards failed with %+v", err)
+			dsp.mu.pools = old
+			return
+		}
+
+		if err := dsp.saveLocked(store); err != nil {
+			logger.Errorf("save shard pool job data failed with %+v", err)
+			dsp.mu.pools = old
+		}
+
+		dsp.triggerCreateLocked()
+	}
+}
+
+func (dsp *dynamicShardsPool) gcAllocating(store storage.JobStorage, aware config.ResourcesAware) {
+	dsp.mu.Lock()
+	defer dsp.mu.Unlock()
+
+	if !dsp.isStartedLocked() {
+		return
+	}
+
+	removed := make(map[uint64][]int)
+	for g, p := range dsp.mu.pools.Pools {
+		var gc []int
+		for idx, allocated := range p.AllocatedShards {
+			stats := aware.GetResource(allocated.ShardID).GetStat()
+			if stats != nil && stats.WrittenKeys > 0 {
+				gc = append(gc, idx)
+			}
+		}
+		removed[g] = gc
+	}
+
+	changed := false
+	for g, ids := range removed {
+		p := dsp.mu.pools.Pools[g]
+
+		if len(ids) > 0 {
+			changed = true
+			newP := p
+			newP.AllocatedShards = newP.AllocatedShards[:0]
+
+			for idx, allocated := range p.AllocatedShards {
+				ok := true
+				for _, id := range ids {
+					if id == idx {
+						ok = false
+					}
+				}
+				if ok {
+					newP.AllocatedShards = append(newP.AllocatedShards, allocated)
+				}
+			}
+
+			dsp.mu.pools.Pools[g] = newP
+		}
+	}
+
+	if changed {
+		dsp.saveLocked(store)
+	}
+}
+
+func (dsp *dynamicShardsPool) saveLocked(store storage.JobStorage) error {
+	err := store.PutJobData(dsp.job, protoc.MustMarshal(&dsp.mu.pools))
+	if err != nil {
+		logger.Errorf("put shards pool metadata to storage failed with %+v", err)
+	}
+	return err
+}
+
+func (dsp *dynamicShardsPool) cloneDataLocked() bhmetapb.ShardsPool {
+	old := bhmetapb.ShardsPool{}
+	protoc.MustUnmarshal(&old, protoc.MustMarshal(&dsp.mu.pools))
+	return old
+}
+
+func (dsp *dynamicShardsPool) unique(g, seq uint64) string {
+	return fmt.Sprintf("%d-%d-%d", dsp.job.Type, g, seq)
+}
+
+func addPrefix(prefix []byte, v uint64) []byte {
+	if len(prefix) == 0 {
+		return format.Uint64ToBytes(v)
+	}
+
+	data := make([]byte, len(prefix)+8)
+	copy(data, prefix)
+	buf.Uint64ToBytesTo(v, data[len(prefix):])
+	return data
+}
---
+++ raftstore/store_shards_pool_test.go
@@ -0,0 +1,100 @@
+package raftstore
+
+import (
+	"testing"
+	"time"
+
+	"github.com/fagongzi/util/protoc"
+	pconfig "github.com/matrixorigin/matrixcube/components/prophet/config"
+	"github.com/matrixorigin/matrixcube/components/prophet/core"
+	"github.com/matrixorigin/matrixcube/components/prophet/metadata"
+	"github.com/matrixorigin/matrixcube/components/prophet/pb/metapb"
+	"github.com/matrixorigin/matrixcube/config"
+	"github.com/matrixorigin/matrixcube/pb/bhmetapb"
+	"github.com/stretchr/testify/assert"
+)
+
+type testResourcesAware struct {
+	aware  pconfig.ResourcesAware
+	adjust func(*core.CachedResource) *core.CachedResource
+}
+
+func (tra *testResourcesAware) ForeachWaittingCreateResources(do func(res metadata.Resource)) {
+	tra.aware.ForeachWaittingCreateResources(do)
+}
+func (tra *testResourcesAware) ForeachResources(group uint64, fn func(res metadata.Resource)) {
+	tra.aware.ForeachResources(group, fn)
+}
+
+func (tra *testResourcesAware) GetResource(resourceID uint64) *core.CachedResource {
+	res := tra.aware.GetResource(resourceID)
+	if tra.adjust != nil {
+		return tra.adjust(res)
+	}
+
+	return res
+}
+
+func TestShardPool(t *testing.T) {
+	c := NewTestClusterStore(t, "", func(cfg *config.Config) {
+		cfg.Customize.CustomInitShardsFactory = func() []bhmetapb.Shard { return []bhmetapb.Shard{{Start: []byte("a"), End: []byte("b")}} }
+	}, nil, nil)
+	defer c.Stop()
+
+	c.Start()
+	c.WaitShardByCount(t, 1, time.Second*10)
+
+	p, err := c.stores[0].CreateResourcePool(metapb.ResourcePool{Group: 0, Capacity: 2, RangePrefix: []byte("b")})
+	assert.NoError(t, err)
+	assert.NotNil(t, p)
+
+	// create 2 shards
+	c.WaitShardByCount(t, 3, time.Second*10)
+
+	allocated, err := p.Alloc(0, []byte("propose1"))
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(1), allocated.AllocatedAt)
+	assert.Equal(t, []byte("propose1"), allocated.Purpose)
+	allocated, err = p.Alloc(0, []byte("propose1"))
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(1), allocated.AllocatedAt)
+	assert.Equal(t, []byte("propose1"), allocated.Purpose)
+
+	// create 3 shards
+	c.WaitShardByCount(t, 4, time.Second*10)
+
+	allocated, err = p.Alloc(0, []byte("propose2"))
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(2), allocated.AllocatedAt)
+	assert.Equal(t, []byte("propose2"), allocated.Purpose)
+
+	// create 4 shards
+	c.WaitShardByCount(t, 5, time.Second*10)
+
+	store := c.GetProphet().GetStorage()
+	v, err := store.GetJobData(metapb.Job{Type: metapb.JobType_CreateResourcePool})
+	assert.NoError(t, err)
+	sp := &bhmetapb.ShardsPool{}
+	protoc.MustUnmarshal(sp, v)
+	assert.Equal(t, uint64(4), sp.Pools[0].Seq)
+	assert.Equal(t, uint64(2), sp.Pools[0].AllocatedOffset)
+	assert.Equal(t, 2, len(sp.Pools[0].AllocatedShards))
+
+	tra := &testResourcesAware{aware: c.GetProphet().GetBasicCluster(), adjust: func(res *core.CachedResource) *core.CachedResource {
+		v := res.Clone(core.SetWrittenKeys(1))
+		return v
+	}}
+	for _, s := range c.stores {
+		if s.shardPool.isStartedLocked() {
+			s.shardPool.gcAllocating(store, tra)
+		}
+	}
+
+	v, err = store.GetJobData(metapb.Job{Type: metapb.JobType_CreateResourcePool})
+	assert.NoError(t, err)
+	sp = &bhmetapb.ShardsPool{}
+	protoc.MustUnmarshal(sp, v)
+	assert.Equal(t, uint64(4), sp.Pools[0].Seq)
+	assert.Equal(t, uint64(2), sp.Pools[0].AllocatedOffset)
+	assert.Equal(t, 0, len(sp.Pools[0].AllocatedShards))
+}
