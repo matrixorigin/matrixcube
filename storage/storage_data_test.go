52f8c7964902425eb9cffd8106a425aeefe45b17
lni <30930154+lni@users.noreply.github.com>
2021-08-18 16:24:15 +0800

    Fix storage tests (#125)

--- storage/storage_data_test.go
+++ storage/storage_data_test.go
@@ -15,7 +15,6 @@ package storage
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 	"testing"
 	"time"
@@ -41,8 +40,8 @@ func createDataMem(fs vfs.FS, t *testing.T) DataStorage {
 
 func createDataPebble(fs vfs.FS, t *testing.T) DataStorage {
 	path := filepath.Join(util.GetTestDir(), "pebble", fmt.Sprintf("%d", time.Now().UnixNano()))
-	os.RemoveAll(path)
-	os.MkdirAll(path, 0755)
+	fs.RemoveAll(path)
+	fs.MkdirAll(path, 0755)
 	opts := &cpebble.Options{FS: vfs.NewPebbleFS(fs)}
 	s, err := pebble.NewStorage(path, opts)
 	assert.NoError(t, err, "createDataPebble failed")
@@ -54,37 +53,35 @@ func TestRangeDelete(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range dataDactories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t).(KVStorage)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("value1")
+			s := factory(fs, t).(KVStorage)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("value1")
 
-				key2 := []byte("k2")
-				value2 := []byte("value2")
+			key2 := []byte("k2")
+			value2 := []byte("value2")
 
-				key3 := []byte("k3")
-				value3 := []byte("value3")
+			key3 := []byte("k3")
+			value3 := []byte("value3")
 
-				assert.NoError(t, s.Set(key1, value1), "TestRangeDelete failed")
-				assert.NoError(t, s.Set(key2, value2), "TestRangeDelete failed")
-				assert.NoError(t, s.Set(key3, value3), "TestRangeDelete failed")
+			assert.NoError(t, s.Set(key1, value1), "TestRangeDelete failed")
+			assert.NoError(t, s.Set(key2, value2), "TestRangeDelete failed")
+			assert.NoError(t, s.Set(key3, value3), "TestRangeDelete failed")
 
-				err := s.RangeDelete(key1, key3)
-				assert.NoError(t, err, "TestRangeDelete failed")
+			err := s.RangeDelete(key1, key3)
+			assert.NoError(t, err, "TestRangeDelete failed")
 
-				value, err := s.Get(key1)
-				assert.NoError(t, err, "TestRangeDelete failed")
-				assert.Equal(t, 0, len(value), "TestRangeDelete failed")
+			value, err := s.Get(key1)
+			assert.NoError(t, err, "TestRangeDelete failed")
+			assert.Equal(t, 0, len(value), "TestRangeDelete failed")
 
-				value, err = s.Get(key2)
-				assert.NoError(t, err, "TestRangeDelete failed")
-				assert.Equal(t, 0, len(value), "TestRangeDelete failed")
+			value, err = s.Get(key2)
+			assert.NoError(t, err, "TestRangeDelete failed")
+			assert.Equal(t, 0, len(value), "TestRangeDelete failed")
 
-				value, err = s.Get(key3)
-				assert.NoError(t, err, "TestRangeDelete failed")
-				assert.Equal(t, len(value3), len(value), "TestRangeDelete failed")
-			}
+			value, err = s.Get(key3)
+			assert.NoError(t, err, "TestRangeDelete failed")
+			assert.Equal(t, len(value3), len(value), "TestRangeDelete failed")
 		})
 	}
 }
@@ -94,22 +91,20 @@ func TestPrefixScan(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range dataDactories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t).(KVStorage)
-				defer s.Close()
-				prefix := "/m/db"
-				for i := 1; i <= 3; i++ {
-					key := []byte(fmt.Sprintf("%v/%v/%d", prefix, "defaultdb", i))
-					err := s.Set(key, []byte{byte(0)})
-					assert.NoError(t, err)
-				}
-				err := s.PrefixScan([]byte(fmt.Sprintf("%v/%v", prefix, "defaultdb")),
-					func(key, value []byte) (bool, error) {
-						println(string(key), value[0])
-						return true, nil
-					}, false)
+			s := factory(fs, t).(KVStorage)
+			defer s.Close()
+			prefix := "/m/db"
+			for i := 1; i <= 3; i++ {
+				key := []byte(fmt.Sprintf("%v/%v/%d", prefix, "defaultdb", i))
+				err := s.Set(key, []byte{byte(0)})
 				assert.NoError(t, err)
 			}
+			err := s.PrefixScan([]byte(fmt.Sprintf("%v/%v", prefix, "defaultdb")),
+				func(key, value []byte) (bool, error) {
+					println(string(key), value[0])
+					return true, nil
+				}, false)
+			assert.NoError(t, err)
 		})
 	}
 }
@@ -119,64 +114,62 @@ func TestSplitCheck(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range dataDactories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				kv := s.(KVStorage)
-				totalSize := uint64(16)
-				totalKeys := uint64(4)
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			s := factory(fs, t)
+			defer s.Close()
+			kv := s.(KVStorage)
+			totalSize := uint64(16)
+			totalKeys := uint64(4)
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key2 := []byte("k2")
-				value2 := []byte("v2")
+			key2 := []byte("k2")
+			value2 := []byte("v2")
 
-				key3 := []byte("k3")
-				value3 := []byte("v3")
+			key3 := []byte("k3")
+			value3 := []byte("v3")
 
-				key4 := []byte("k4")
-				value4 := []byte("v4")
+			key4 := []byte("k4")
+			value4 := []byte("v4")
 
-				end := []byte("k5")
+			end := []byte("k5")
 
-				assert.NoError(t, kv.Set(key1, value1))
-				assert.NoError(t, kv.Set(key2, value2))
-				assert.NoError(t, kv.Set(key3, value3))
-				assert.NoError(t, kv.Set(key4, value4))
+			assert.NoError(t, kv.Set(key1, value1))
+			assert.NoError(t, kv.Set(key2, value2))
+			assert.NoError(t, kv.Set(key3, value3))
+			assert.NoError(t, kv.Set(key4, value4))
 
-				// [key1, key5), after split ranges: [key1, key2), [key2, key3), [key3, key4), [key4, key5)
-				total, keys, splitKeys, err := s.SplitCheck(key1, end, 4)
-				assert.NoError(t, err)
-				assert.Equal(t, totalSize, total)
-				assert.Equal(t, totalKeys, keys)
-				assert.Equal(t, 3, len(splitKeys))
-				assert.Equal(t, key2, splitKeys[0])
-				assert.Equal(t, key3, splitKeys[1])
-				assert.Equal(t, key4, splitKeys[2])
+			// [key1, key5), after split ranges: [key1, key2), [key2, key3), [key3, key4), [key4, key5)
+			total, keys, splitKeys, err := s.SplitCheck(key1, end, 4)
+			assert.NoError(t, err)
+			assert.Equal(t, totalSize, total)
+			assert.Equal(t, totalKeys, keys)
+			assert.Equal(t, 3, len(splitKeys))
+			assert.Equal(t, key2, splitKeys[0])
+			assert.Equal(t, key3, splitKeys[1])
+			assert.Equal(t, key4, splitKeys[2])
 
-				// [key1, key5), after split ranges: [key1, key3), [key3, key5)
-				total, keys, splitKeys, err = s.SplitCheck(key1, end, 8)
-				assert.NoError(t, err)
-				assert.Equal(t, totalSize, total)
-				assert.Equal(t, totalKeys, keys)
-				assert.Equal(t, 1, len(splitKeys))
-				assert.Equal(t, key3, splitKeys[0])
+			// [key1, key5), after split ranges: [key1, key3), [key3, key5)
+			total, keys, splitKeys, err = s.SplitCheck(key1, end, 8)
+			assert.NoError(t, err)
+			assert.Equal(t, totalSize, total)
+			assert.Equal(t, totalKeys, keys)
+			assert.Equal(t, 1, len(splitKeys))
+			assert.Equal(t, key3, splitKeys[0])
 
-				// [key1, key5), after split ranges: [key1, key4), [key4, key5)
-				total, keys, splitKeys, err = s.SplitCheck(key1, end, 12)
-				assert.NoError(t, err)
-				assert.Equal(t, totalSize, total)
-				assert.Equal(t, totalKeys, keys)
-				assert.Equal(t, 1, len(splitKeys))
-				assert.Equal(t, key4, splitKeys[0])
+			// [key1, key5), after split ranges: [key1, key4), [key4, key5)
+			total, keys, splitKeys, err = s.SplitCheck(key1, end, 12)
+			assert.NoError(t, err)
+			assert.Equal(t, totalSize, total)
+			assert.Equal(t, totalKeys, keys)
+			assert.Equal(t, 1, len(splitKeys))
+			assert.Equal(t, key4, splitKeys[0])
 
-				// [key1, key5), after split ranges: [key1, key5)
-				total, keys, splitKeys, err = s.SplitCheck(key1, end, 16)
-				assert.NoError(t, err)
-				assert.Equal(t, totalSize, total)
-				assert.Equal(t, totalKeys, keys)
-				assert.Equal(t, 0, len(splitKeys))
-			}
+			// [key1, key5), after split ranges: [key1, key5)
+			total, keys, splitKeys, err = s.SplitCheck(key1, end, 16)
+			assert.NoError(t, err)
+			assert.Equal(t, totalSize, total)
+			assert.Equal(t, totalKeys, keys)
+			assert.Equal(t, 0, len(splitKeys))
 		})
 	}
 }
@@ -186,50 +179,48 @@ func TestCreateAndApply(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range dataDactories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s1 := factory(fs, t)
-				defer s1.Close()
-				kv1 := s1.(KVStorage)
+			s1 := factory(fs, t)
+			defer s1.Close()
+			kv1 := s1.(KVStorage)
+			s2 := factory(fs, t)
+			defer s2.Close()
+			kv2 := s2.(KVStorage)
+			path := fmt.Sprintf("%s-snap", name)
+			path = filepath.Join(util.GetTestDir(), path)
+			fs.RemoveAll(path)
+			err := fs.MkdirAll(path, 0755)
+			assert.NoError(t, err, "TestCreateAndApply failed")
 
-				s2 := factory(fs, t)
-				kv2 := s2.(KVStorage)
-				path := fmt.Sprintf("%s-snap", name)
-				path = filepath.Join(util.GetTestDir(), path)
-				os.RemoveAll(path)
-				err := os.MkdirAll(path, 0755)
-				assert.NoError(t, err, "TestCreateAndApply failed")
+			key1 := []byte("key1")
+			value1 := []byte("value1")
 
-				key1 := []byte("key1")
-				value1 := []byte("value1")
+			key2 := []byte("key2")
+			value2 := []byte("value2")
 
-				key2 := []byte("key2")
-				value2 := []byte("value2")
+			assert.NoError(t, kv1.Set(key1, value1), "TestCreateAndApply failed")
+			assert.NoError(t, kv1.Set(key2, value2), "TestCreateAndApply failed")
 
-				assert.NoError(t, kv1.Set(key1, value1), "TestCreateAndApply failed")
-				assert.NoError(t, kv1.Set(key2, value2), "TestCreateAndApply failed")
+			err = s1.CreateSnapshot(path, key1, key2)
+			assert.NoError(t, err, "TestCreateAndApply failed")
 
-				err = s1.CreateSnapshot(path, key1, key2)
-				assert.NoError(t, err, "TestCreateAndApply failed")
+			key4 := []byte("key4")
+			value4 := []byte("value4")
+			assert.NoError(t, kv2.Set(key4, value4), "TestCreateAndApply failed")
 
-				key4 := []byte("key4")
-				value4 := []byte("value4")
-				assert.NoError(t, kv2.Set(key4, value4), "TestCreateAndApply failed")
+			err = s2.ApplySnapshot(path)
+			assert.NoError(t, err, "TestCreateAndApply failed")
 
-				err = s2.ApplySnapshot(path)
-				assert.NoError(t, err, "TestCreateAndApply failed")
+			value, err := kv2.Get(key2)
+			assert.NoError(t, err, "TestCreateAndApply failed")
+			assert.Equal(t, 0, len(value), "TestCreateAndApply failed")
 
-				value, err := kv2.Get(key2)
-				assert.NoError(t, err, "TestCreateAndApply failed")
-				assert.Equal(t, 0, len(value), "TestCreateAndApply failed")
+			value, err = kv2.Get(key4)
+			assert.NoError(t, err, "TestCreateAndApply failed")
+			assert.Equal(t, len(value4), len(value), "TestCreateAndApply failed")
 
-				value, err = kv2.Get(key4)
-				assert.NoError(t, err, "TestCreateAndApply failed")
-				assert.Equal(t, len(value4), len(value), "TestCreateAndApply failed")
-
-				value, err = kv2.Get(key1)
-				assert.NoError(t, err, "TestCreateAndApply failed")
-				assert.Equal(t, string(value1), string(value), "TestCreateAndApply failed")
-			}
+			value, err = kv2.Get(key1)
+			assert.NoError(t, err, "TestCreateAndApply failed")
+			assert.Equal(t, string(value1), string(value), "TestCreateAndApply failed")
 		})
 	}
 }
--- storage/storage_meta_test.go
+++ storage/storage_meta_test.go
@@ -15,7 +15,6 @@ package storage
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 	"testing"
 	"time"
@@ -41,8 +40,8 @@ func createMem(fs vfs.FS, t *testing.T) MetadataStorage {
 
 func createPebble(fs vfs.FS, t *testing.T) MetadataStorage {
 	path := filepath.Join(util.GetTestDir(), "pebble", fmt.Sprintf("%d", time.Now().UnixNano()))
-	os.RemoveAll(path)
-	os.MkdirAll(path, 0755)
+	fs.RemoveAll(path)
+	fs.MkdirAll(path, 0755)
 	opts := &cpebble.Options{FS: vfs.NewPebbleFS(fs)}
 	s, err := pebble.NewStorage(path, opts)
 	assert.NoError(t, err, "createPebble failed")
@@ -54,48 +53,46 @@ func TestWriteBatch(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				wb := util.NewWriteBatch()
+			s := factory(fs, t)
+			defer s.Close()
+			wb := util.NewWriteBatch()
 
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key2 := []byte("k2")
-				value2 := []byte("v2")
+			key2 := []byte("k2")
+			value2 := []byte("v2")
 
-				key3 := []byte("k3")
-				value3 := []byte("v3")
+			key3 := []byte("k3")
+			value3 := []byte("v3")
 
-				key4 := []byte("k4")
-				value4 := []byte("v4")
+			key4 := []byte("k4")
+			value4 := []byte("v4")
 
-				wb.Set(key1, value1)
-				wb.Set(key4, value4)
-				wb.Delete(key4)
-				wb.Set(key2, value2)
-				wb.Set(key3, value3)
+			wb.Set(key1, value1)
+			wb.Set(key4, value4)
+			wb.Delete(key4)
+			wb.Set(key2, value2)
+			wb.Set(key3, value3)
 
-				err := s.Write(wb, true)
-				assert.NoError(t, err, "TestWriteBatch failed")
+			err := s.Write(wb, true)
+			assert.NoError(t, err, "TestWriteBatch failed")
 
-				value, err := s.Get(key1)
-				assert.NoError(t, err, "TestWriteBatch failed")
-				assert.Equal(t, string(value1), string(value), "TestWriteBatch failed")
+			value, err := s.Get(key1)
+			assert.NoError(t, err, "TestWriteBatch failed")
+			assert.Equal(t, string(value1), string(value), "TestWriteBatch failed")
 
-				value, err = s.Get(key2)
-				assert.NoError(t, err, "TestWriteBatch failed")
-				assert.Equal(t, string(value2), string(value), "TestWriteBatch failed")
+			value, err = s.Get(key2)
+			assert.NoError(t, err, "TestWriteBatch failed")
+			assert.Equal(t, string(value2), string(value), "TestWriteBatch failed")
 
-				value, err = s.Get(key3)
-				assert.NoError(t, err, "TestWriteBatch failed")
-				assert.Equal(t, string(value3), string(value), "TestWriteBatch failed")
+			value, err = s.Get(key3)
+			assert.NoError(t, err, "TestWriteBatch failed")
+			assert.Equal(t, string(value3), string(value), "TestWriteBatch failed")
 
-				value, err = s.Get(key4)
-				assert.NoError(t, err, "TestWriteBatch failed")
-				assert.Equal(t, "", string(value), "TestWriteBatch failed")
-			}
+			value, err = s.Get(key4)
+			assert.NoError(t, err, "TestWriteBatch failed")
+			assert.Equal(t, "", string(value), "TestWriteBatch failed")
 		})
 	}
 }
@@ -105,40 +102,38 @@ func TestSetAndGet(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key2 := []byte("k2")
-				value2 := []byte("v2")
+			key2 := []byte("k2")
+			value2 := []byte("v2")
 
-				key3 := []byte("k3")
-				value3 := []byte("v3")
+			key3 := []byte("k3")
+			value3 := []byte("v3")
 
-				key4 := []byte("k4")
+			key4 := []byte("k4")
 
-				s.Set(key1, value1)
-				s.Set(key2, value2)
-				s.Set(key3, value3)
+			s.Set(key1, value1)
+			s.Set(key2, value2)
+			s.Set(key3, value3)
 
-				value, err := s.Get(key1)
-				assert.NoError(t, err, "TestSetAndGet failed")
-				assert.Equal(t, string(value1), string(value), "TestSetAndGet failed")
+			value, err := s.Get(key1)
+			assert.NoError(t, err, "TestSetAndGet failed")
+			assert.Equal(t, string(value1), string(value), "TestSetAndGet failed")
 
-				value, err = s.Get(key2)
-				assert.NoError(t, err, "TestSetAndGet failed")
-				assert.Equal(t, string(value2), string(value), "TestSetAndGet failed")
+			value, err = s.Get(key2)
+			assert.NoError(t, err, "TestSetAndGet failed")
+			assert.Equal(t, string(value2), string(value), "TestSetAndGet failed")
 
-				value, err = s.Get(key3)
-				assert.NoError(t, err, "TestSetAndGet failed")
-				assert.Equal(t, string(value3), string(value), "TestSetAndGet failed")
+			value, err = s.Get(key3)
+			assert.NoError(t, err, "TestSetAndGet failed")
+			assert.Equal(t, string(value3), string(value), "TestSetAndGet failed")
 
-				value, err = s.Get(key4)
-				assert.NoError(t, err, "TestSetAndGet failed")
-				assert.Equal(t, "", string(value), "TestSetAndGet failed")
-			}
+			value, err = s.Get(key4)
+			assert.NoError(t, err, "TestSetAndGet failed")
+			assert.Equal(t, "", string(value), "TestSetAndGet failed")
 		})
 	}
 }
@@ -148,33 +143,31 @@ func TestSetAndMGet(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key2 := []byte("k2")
-				value2 := []byte("v2")
+			key2 := []byte("k2")
+			value2 := []byte("v2")
 
-				key3 := []byte("k3")
-				value3 := []byte("v3")
+			key3 := []byte("k3")
+			value3 := []byte("v3")
 
-				key4 := []byte("k4")
+			key4 := []byte("k4")
 
-				s.Set(key1, value1)
-				s.Set(key2, value2)
-				s.Set(key3, value3)
+			s.Set(key1, value1)
+			s.Set(key2, value2)
+			s.Set(key3, value3)
 
-				values, err := s.MGet(key1, key2, key3, key4)
-				assert.NoError(t, err, "TestSetAndGet failed")
-				assert.Equal(t, 4, len(values), "TestSetAndGet failed")
+			values, err := s.MGet(key1, key2, key3, key4)
+			assert.NoError(t, err, "TestSetAndGet failed")
+			assert.Equal(t, 4, len(values), "TestSetAndGet failed")
 
-				assert.Equal(t, string(value1), string(values[0]), "TestSetAndGet failed")
-				assert.Equal(t, string(value2), string(values[1]), "TestSetAndGet failed")
-				assert.Equal(t, string(value3), string(values[2]), "TestSetAndGet failed")
-				assert.Equal(t, 0, len(values[3]), "TestSetAndGet failed")
-			}
+			assert.Equal(t, string(value1), string(values[0]), "TestSetAndGet failed")
+			assert.Equal(t, string(value2), string(values[1]), "TestSetAndGet failed")
+			assert.Equal(t, string(value3), string(values[2]), "TestSetAndGet failed")
+			assert.Equal(t, 0, len(values[3]), "TestSetAndGet failed")
 		})
 	}
 }
@@ -184,35 +177,33 @@ func TestSetAndGetWithTTL(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				if name == "pebble" {
-					return
-				}
-
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
-
-				s.SetWithTTL(key1, value1, 1)
-
-				value, err := s.Get(key1)
-				assert.NoError(t, err, "TestSetAndGetWithTTL failed")
-				assert.Equal(t, string(value1), string(value), "TestSetAndGetWithTTL failed")
-
-				time.Sleep(time.Millisecond * 1200)
-				value, err = s.Get(key1)
-				assert.NoError(t, err, "TestSetAndGetWithTTL failed")
-				assert.Equal(t, 0, len(value), "TestSetAndGetWithTTL failed")
-
-				c := 0
-				err = s.Scan(key1, []byte("k2"), func(key, value []byte) (bool, error) {
-					c++
-					return true, nil
-				}, false)
-				assert.NoError(t, err, "TestSetAndGetWithTTL failed")
-				assert.Equal(t, 0, c, "TestSetAndGetWithTTL failed")
+			if name == "pebble" {
+				return
 			}
+
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
+
+			s.SetWithTTL(key1, value1, 1)
+
+			value, err := s.Get(key1)
+			assert.NoError(t, err, "TestSetAndGetWithTTL failed")
+			assert.Equal(t, string(value1), string(value), "TestSetAndGetWithTTL failed")
+
+			time.Sleep(time.Millisecond * 1200)
+			value, err = s.Get(key1)
+			assert.NoError(t, err, "TestSetAndGetWithTTL failed")
+			assert.Equal(t, 0, len(value), "TestSetAndGetWithTTL failed")
+
+			c := 0
+			err = s.Scan(key1, []byte("k2"), func(key, value []byte) (bool, error) {
+				c++
+				return true, nil
+			}, false)
+			assert.NoError(t, err, "TestSetAndGetWithTTL failed")
+			assert.Equal(t, 0, c, "TestSetAndGetWithTTL failed")
 		})
 	}
 }
@@ -222,38 +213,36 @@ func TestWritebatchWithTTL(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				if name == "pebble" {
-					return
-				}
-
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
-				wb := util.NewWriteBatch()
-				wb.SetWithTTL(key1, value1, 1)
-
-				err := s.Write(wb, false)
-				assert.NoError(t, err, "TestWritebatchWithTTL failed")
-
-				value, err := s.Get(key1)
-				assert.NoError(t, err, "TestWritebatchWithTTL failed")
-				assert.Equal(t, string(value1), string(value), "TestWritebatchWithTTL failed")
-
-				time.Sleep(time.Millisecond * 1200)
-				value, err = s.Get(key1)
-				assert.NoError(t, err, "TestWritebatchWithTTL failed")
-				assert.Equal(t, 0, len(value), "TestWritebatchWithTTL failed")
-
-				c := 0
-				err = s.Scan(key1, []byte("k2"), func(key, value []byte) (bool, error) {
-					c++
-					return true, nil
-				}, false)
-				assert.NoError(t, err, "TestWritebatchWithTTL failed")
-				assert.Equal(t, 0, c, "TestWritebatchWithTTL failed")
+			if name == "pebble" {
+				return
 			}
+
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
+			wb := util.NewWriteBatch()
+			wb.SetWithTTL(key1, value1, 1)
+
+			err := s.Write(wb, false)
+			assert.NoError(t, err, "TestWritebatchWithTTL failed")
+
+			value, err := s.Get(key1)
+			assert.NoError(t, err, "TestWritebatchWithTTL failed")
+			assert.Equal(t, string(value1), string(value), "TestWritebatchWithTTL failed")
+
+			time.Sleep(time.Millisecond * 1200)
+			value, err = s.Get(key1)
+			assert.NoError(t, err, "TestWritebatchWithTTL failed")
+			assert.Equal(t, 0, len(value), "TestWritebatchWithTTL failed")
+
+			c := 0
+			err = s.Scan(key1, []byte("k2"), func(key, value []byte) (bool, error) {
+				c++
+				return true, nil
+			}, false)
+			assert.NoError(t, err, "TestWritebatchWithTTL failed")
+			assert.Equal(t, 0, c, "TestWritebatchWithTTL failed")
 		})
 	}
 }
@@ -263,28 +252,26 @@ func TestDelete(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key2 := []byte("k2")
-				value2 := []byte("v2")
+			key2 := []byte("k2")
+			value2 := []byte("v2")
 
-				s.Set(key1, value1)
-				s.Set(key2, value2)
+			s.Set(key1, value1)
+			s.Set(key2, value2)
 
-				s.Delete(key2)
+			s.Delete(key2)
 
-				value, err := s.Get(key1)
-				assert.NoError(t, err, "TestDelete failed")
-				assert.Equal(t, string(value1), string(value), "TestDelete failed")
+			value, err := s.Get(key1)
+			assert.NoError(t, err, "TestDelete failed")
+			assert.Equal(t, string(value1), string(value), "TestDelete failed")
 
-				value, err = s.Get(key2)
-				assert.NoError(t, err, "TestDelete failed")
-				assert.Equal(t, "", string(value), "TestDelete failed")
-			}
+			value, err = s.Get(key2)
+			assert.NoError(t, err, "TestDelete failed")
+			assert.Equal(t, "", string(value), "TestDelete failed")
 		})
 	}
 }
@@ -294,37 +281,35 @@ func TestMetaRangeDelete(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key2 := []byte("k2")
-				value2 := []byte("v2")
+			key2 := []byte("k2")
+			value2 := []byte("v2")
 
-				key3 := []byte("k3")
-				value3 := []byte("v3")
+			key3 := []byte("k3")
+			value3 := []byte("v3")
 
-				s.Set(key1, value1)
-				s.Set(key2, value2)
-				s.Set(key3, value3)
+			s.Set(key1, value1)
+			s.Set(key2, value2)
+			s.Set(key3, value3)
 
-				err := s.RangeDelete(key1, key3)
-				assert.NoError(t, err, "TestMetaRangeDelete failed")
+			err := s.RangeDelete(key1, key3)
+			assert.NoError(t, err, "TestMetaRangeDelete failed")
 
-				value, err := s.Get(key1)
-				assert.NoError(t, err, "TestMetaRangeDelete failed")
-				assert.Equal(t, "", string(value), "TestMetaRangeDelete failed")
+			value, err := s.Get(key1)
+			assert.NoError(t, err, "TestMetaRangeDelete failed")
+			assert.Equal(t, "", string(value), "TestMetaRangeDelete failed")
 
-				value, err = s.Get(key2)
-				assert.NoError(t, err, "TestMetaRangeDelete failed")
-				assert.Equal(t, "", string(value), "TestMetaRangeDelete failed")
+			value, err = s.Get(key2)
+			assert.NoError(t, err, "TestMetaRangeDelete failed")
+			assert.Equal(t, "", string(value), "TestMetaRangeDelete failed")
 
-				value, err = s.Get(key3)
-				assert.NoError(t, err, "TestMetaRangeDelete failed")
-				assert.Equal(t, string(value3), string(value), "TestMetaRangeDelete failed")
-			}
+			value, err = s.Get(key3)
+			assert.NoError(t, err, "TestMetaRangeDelete failed")
+			assert.Equal(t, string(value3), string(value), "TestMetaRangeDelete failed")
 		})
 	}
 }
@@ -334,28 +319,26 @@ func TestSeek(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key3 := []byte("k3")
-				value3 := []byte("v3")
+			key3 := []byte("k3")
+			value3 := []byte("v3")
 
-				s.Set(key1, value1)
-				s.Set(key3, value3)
+			s.Set(key1, value1)
+			s.Set(key3, value3)
 
-				key, value, err := s.Seek(key1)
-				assert.NoError(t, err, "TestSeek failed")
-				assert.Equal(t, string(key1), string(key), "TestSeek failed")
-				assert.Equal(t, string(value1), string(value), "TestSeek failed")
+			key, value, err := s.Seek(key1)
+			assert.NoError(t, err, "TestSeek failed")
+			assert.Equal(t, string(key1), string(key), "TestSeek failed")
+			assert.Equal(t, string(value1), string(value), "TestSeek failed")
 
-				key, value, err = s.Seek([]byte("k2"))
-				assert.NoError(t, err, "TestSeek failed")
-				assert.Equal(t, string(key3), string(key), "TestSeek failed")
-				assert.Equal(t, string(value3), string(value), "TestSeek failed")
-			}
+			key, value, err = s.Seek([]byte("k2"))
+			assert.NoError(t, err, "TestSeek failed")
+			assert.Equal(t, string(key3), string(key), "TestSeek failed")
+			assert.Equal(t, string(value3), string(value), "TestSeek failed")
 		})
 	}
 }
@@ -365,41 +348,39 @@ func TestScan(t *testing.T) {
 	defer vfs.ReportLeakedFD(fs, t)
 	for name, factory := range factories {
 		t.Run(name, func(t *testing.T) {
-			{
-				s := factory(fs, t)
-				defer s.Close()
-				key1 := []byte("k1")
-				value1 := []byte("v1")
+			s := factory(fs, t)
+			defer s.Close()
+			key1 := []byte("k1")
+			value1 := []byte("v1")
 
-				key2 := []byte("k2")
-				value2 := []byte("v2")
+			key2 := []byte("k2")
+			value2 := []byte("v2")
 
-				key3 := []byte("k3")
-				value3 := []byte("v3")
+			key3 := []byte("k3")
+			value3 := []byte("v3")
 
-				s.Set(key1, value1)
-				s.Set(key2, value2)
-				s.Set(key3, value3)
+			s.Set(key1, value1)
+			s.Set(key2, value2)
+			s.Set(key3, value3)
 
-				count := 0
-				err := s.Scan(key1, key3, func(key, value []byte) (bool, error) {
-					count++
-					return true, nil
-				}, false)
-				assert.NoError(t, err, "TestScan failed")
-				assert.Equal(t, 2, count, "TestScan failed")
+			count := 0
+			err := s.Scan(key1, key3, func(key, value []byte) (bool, error) {
+				count++
+				return true, nil
+			}, false)
+			assert.NoError(t, err, "TestScan failed")
+			assert.Equal(t, 2, count, "TestScan failed")
 
-				count = 0
-				err = s.Scan(key1, key3, func(key, value []byte) (bool, error) {
-					count++
-					if count == 1 {
-						return false, nil
-					}
-					return true, nil
-				}, false)
-				assert.NoError(t, err, "TestScan failed")
-				assert.Equal(t, 1, count, "TestScan failed")
-			}
+			count = 0
+			err = s.Scan(key1, key3, func(key, value []byte) (bool, error) {
+				count++
+				if count == 1 {
+					return false, nil
+				}
+				return true, nil
+			}, false)
+			assert.NoError(t, err, "TestScan failed")
+			assert.Equal(t, 1, count, "TestScan failed")
 		})
 	}
 }
