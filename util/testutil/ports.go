84de26a13f604ba9e741e8f0dac41ffcede5fadd
fagongzi <zhangxu19830126@gmail.com>
2021-09-08 21:21:03 +0800

    Fix #221 upgrade goetty support SO_REUSEPORT (#224)
    * Fix #221 upgrade goetty support SO_REUSEPORT
    
    * Remove unnecessary case

---
+++ util/testutil/ports.go
@@ -0,0 +1,22 @@
+package testutil
+
+import (
+	"sync"
+)
+
+var (
+	mutex sync.Mutex
+	port  = 10000
+)
+
+// GenTestPorts gen and return ports
+func GenTestPorts(n int) []int {
+	mutex.Lock()
+	defer mutex.Unlock()
+	ports := make([]int, n)
+	for i := 0; i < n; i++ {
+		ports[i] = port
+		port++
+	}
+	return ports
+}
