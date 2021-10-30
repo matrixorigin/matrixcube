fb51bdb7b49442754ba02be887ffade81693e762
Lei Ni <nilei81@gmail.com>
2021-10-30 11:28:44 +0800

    added some methods

---
+++ raftstore/replica_snapshot_env.go
@@ -0,0 +1,33 @@
+// Copyright 2021 MatrixOrigin.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package raftstore
+
+import (
+	"fmt"
+)
+
+const (
+	// DO NOT CHANGE
+	snapshotDirName = "snapshots"
+)
+
+func (r *replica) snapshotRootDir() string {
+	fs := r.cfg.FS
+	replicaDir := fmt.Sprintf("shard-%d-replica-%d", r.shardID, r.replica.ID)
+	return fs.PathJoin(r.cfg.DataPath, snapshotDirName, replicaDir)
+}
+
+func (r *replica) prepareSnapshotDir() error {
+	return nil
+}
