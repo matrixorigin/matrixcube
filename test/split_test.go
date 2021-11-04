048f64f272d03524ff01711da42bbf2f2cabe1a4
zhangxu <zhangxu19830126@gmail.com>
2021-11-04 09:40:22 +0800

    Add more check for split test

--- test/split_test.go
+++ test/split_test.go
@@ -23,20 +23,13 @@ func TestSplitWithSingleCluster(t *testing.T) {
 	c.Start()
 	defer c.Stop()
 
-	c.WaitShardByCountPerNode(1, testWaitTimeout)
-
-	kv := c.CreateTestKVClient(0)
-	defer kv.Close()
-
-	sid := c.GetShardByIndex(0, 0).ID
-	kv.Set("k1", "v1", testWaitTimeout)
-	kv.Set("k2", "v2", testWaitTimeout)
-
-	c.WaitShardSplitByCount(sid, 1, testWaitTimeout)
-	c.WaitRemovedByShardID(sid, testWaitTimeout)
-
+	sid := prepareSplit(t, c)
 	c.Restart()
 	checkSplitWithStore(t, c, 0, sid)
+
+	v, err := c.GetStore(0).Prophet().GetStorage().GetResource(sid)
+	assert.NoError(t, err)
+	assert.Nil(t, v)
 }
 
 func TestSplitWithMultiNodesCluster(t *testing.T) {
@@ -52,6 +45,18 @@ func TestSplitWithMultiNodesCluster(t *testing.T) {
 	c.Start()
 	defer c.Stop()
 
+	sid := prepareSplit(t, c)
+	c.Restart()
+	c.EveryStore(func(index int, store raftstore.Store) {
+		checkSplitWithStore(t, c, index, sid)
+	})
+
+	v, err := c.GetStore(0).Prophet().GetStorage().GetResource(sid)
+	assert.NoError(t, err)
+	assert.Nil(t, v)
+}
+
+func prepareSplit(t *testing.T, c raftstore.TestRaftCluster) uint64 {
 	c.WaitShardByCountPerNode(1, testWaitTimeout)
 
 	kv := c.CreateTestKVClient(0)
@@ -66,11 +71,7 @@ func TestSplitWithMultiNodesCluster(t *testing.T) {
 
 	// new shards created
 	c.WaitShardByCountPerNode(2, testWaitTimeout)
-
-	c.Restart()
-	c.EveryStore(func(index int, store raftstore.Store) {
-		checkSplitWithStore(t, c, index, sid)
-	})
+	return sid
 }
 
 func checkSplitWithStore(t *testing.T, c raftstore.TestRaftCluster, index int, parentShardID uint64) {
@@ -91,4 +92,10 @@ func checkSplitWithStore(t *testing.T, c raftstore.TestRaftCluster, index int, p
 	v, err = kv.Get("k2", testWaitTimeout)
 	assert.NoError(t, err, "index %d", index)
 	assert.Equal(t, "v2", string(v), "index %d", index)
+
+	// check router
+	shard, _ := c.GetStore(index).GetRouter().SelectShard(0, []byte("k1"))
+	assert.NotEqual(t, parentShardID, shard.ID)
+	shard, _ = c.GetStore(index).GetRouter().SelectShard(0, []byte("k2"))
+	assert.NotEqual(t, parentShardID, shard.ID)
 }
