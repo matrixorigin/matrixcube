matrixorigin/matrixcube

commit becf7c204fe655dd2e949b708fea44d3f82718a7

Resolve #374 allow concurrently creating snapshots at the same index (#375)
* allow concurrently creating the same snapshot
* include extra info into the generated raftpb.Snapshot object

raftstore/replica_event_loop.go
@@ -179,8 +179,7 @@ func (pr *replica) handleEvent(wc *logdb.WorkerContext) (hasEvent bool) {
 	if pr.handleAction(pr.items) {
 		hasEvent = true
 	}
-
-	return hasEvent
+	return true
 }
 
 func (pr *replica) handleAction(items []interface{}) bool {

pb/meta/meta.proto
@@ -159,3 +159,8 @@ message ShardsPoolAllocCmd {
     uint64 group   = 1;
     bytes  purpose = 2;
 }
+
+// SnapshotInfo contains additional information associated with a snapshot.
+message SnapshotInfo {
+  uint64 extra = 1;
+}

raftstore/log_reader.go
@@ -262,7 +262,7 @@ func (lr *LogReader) CreateSnapshot(snapshot pb.Snapshot) error {
 }
 
 func (lr *LogReader) setSnapshot(snapshot pb.Snapshot) error {
-	if lr.snapshot.Metadata.Index >= snapshot.Metadata.Index {
+	if lr.snapshot.Metadata.Index > snapshot.Metadata.Index {
 		lr.logger.Debug("called setSnapshot",
 			zap.String("id", lr.id()),
 			zap.Uint64("existing", lr.snapshot.Metadata.Index),

commit d118d77265e80506d09b0fef2f02730d9b515bcf

Fix #417 fixed high cpu load issue caused by bad merge (#418)
caused in becf7c204fe655dd2e949b708fea44d3f82718a7

raftstore/replica_event_loop.go
@@ -194,7 +194,7 @@ func (pr *replica) handleEvent(wc *logdb.WorkerContext) (hasEvent bool, err erro
 	if pr.handleAction(pr.items) {
 		hasEvent = true
 	}
-	return true, nil
+	return hasEvent, nil
 }
 
 func (pr *replica) handleAction(items []interface{}) bool {