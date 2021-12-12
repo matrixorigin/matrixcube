55922beaaee0bad431e3a2f5e1cfcc73a6b5e021
zhangxu <zhangxu19830126@gmail.com>
2021-12-12 20:38:33 +0800

    Support run testcase in docker

--- test.sh
+++ test.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+doTesting(){
+    base=$1
+    dir=$2
+    for i in {1..50000}
+    do
+        echo "docker run -e RACE=1 -e MallocNanoZone=0  -it -v $base:/matrixcube matrixorigin/matrixcube-test all-tests > $dir/test.log" 
+        docker run -e RACE=1 -e MallocNanoZone=0 -i -v $base:/matrixcube matrixorigin/matrixcube-test all-tests > $dir/test.log
+        v=`tail -n 1 $dir/test.log | awk {'print $1'}`
+        if [ "$v" != "ok" ]
+        then
+            mv $dir/test.log $dir/test-$i.log
+            echo "$i: error" >> $dir/result.log
+        else
+            echo "$i: ok" >> $dir/result.log
+        fi
+    done
+}
 
-for i in {1..50000}
+for i in `seq 1 $1`
 do
-    go test -timeout 600s -count 1 -v github.com/matrixorigin/matrixcube/raftstore > test.log
-    v=`tail -n 1 test.log | awk {'print $1'}`
-    if [ "$v" != "ok" ]
-    then
-        mv ./test.log ./test-$i.log
-        echo "$i: error"
-    else
-        echo "$i: ok"
-    fi
+    mkdir -p $PWD/tests/$i
+    doTesting $PWD $PWD/tests/$i &
 done
