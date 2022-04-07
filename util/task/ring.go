9c959310fc887880015645bbe082d81db67eb9d2
zhangxu <zhangxu19830126@gmail.com>
2022-04-07 08:43:18 +0800

    fix more static check

--- util/task/ring.go
+++ util/task/ring.go
@@ -47,13 +47,13 @@ type nodes []*node
 // described here: http://www.1024cores.net/home/lock-free-algorithms/queues/bounded-mpmc-queue
 // with some minor additions.
 type RingBuffer struct {
-	_padding0      [8]uint64
+	_              [8]uint64
 	queue          uint64
-	_padding1      [8]uint64
+	_              [8]uint64
 	dequeue        uint64
-	_padding2      [8]uint64
+	_              [8]uint64
 	mask, disposed uint64
-	_padding3      [8]uint64
+	_              [8]uint64
 	nodes          nodes
 }
 
